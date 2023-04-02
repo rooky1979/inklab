05da5914e983d4e71d110a50ca83a5526237aeee
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-04-03 10:09:52 +1200

    Test overlay text on landing page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-
 import landingStyles from "@/styles/Landing.module.css";
 
 export default function Landing() {
   return (
     <>
-    <h1 className={landingStyles.heading} >The Ink Lab</h1>
-      <video autoPlay loop className={landingStyles.video}>
+      <video autoPlay controls loop className={landingStyles.video}>
         <source src="./inklabLanding.mp4" type="video/mp4" />
       </video>
+      <h1 className={landingStyles.heading}>The Ink Lab</h1>
     </>
   );
 }
