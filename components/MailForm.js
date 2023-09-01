c50ce790d29d0e8197303c349ac5bf90417fa80e
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-09-01 20:51:05 +1200

    Custom mail form

--- components/MailForm.js
+++ components/MailForm.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
 import mailingStyles from "@/styles/Mailing.module.css";
-import MailchimpSubscribe from 'react-mailchimp-subscribe';
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 
-const MailForm = props => {
-
-const posturl = `https://hotmail.us21.list-manage.com/subscribe/post?=${TOBEADDED}&id=${TOBEADDED}`;
+const MailForm = (props) => {
+  const postUrl = `https://hotmail.us21.list-manage.com/subscribe/post?=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
   return (
     <div className={mailingStyles.formcontainer}>
-      <MailchimpSubscribe />
+      <MailchimpSubscribe
+        url={postUrl}
+        render={({ subscribe, status, message }) => (
+          <CustomForm
+            status={status}
+            message={message}
+            onValidated={(formData) => subscribe(formData)}
+          />
+        )}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MailForm
+export default MailForm;
