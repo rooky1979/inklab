a5541f7dd955576d6007a2777fe4023606a506d9
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-09-03 11:09:56 +1200

    Styling mailing

--- components/SubscribeForm.js
+++ components/SubscribeForm.js
@@ -34,43 +34,77 @@ const SubscribeForm = ({ backgroundImage }) => {
   };
   return (
     <div className={formStyles.mainpage} style={mainpageStyle}>
-      <div className={formStyles.bodycontainer}>
-        <form
-          className={formStyles.formcontainer}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className={formStyles.formfield}>
-            <label className={formStyles.label}>First Name:</label>
-            <input
-              className={formStyles.input}
-              type="text"
-              {...register("firstName")}
-            />
-          </div>
-          <br />
-          <div className={formStyles.formfield}>
-            <label className={formStyles.label}>Last Name:</label>
-            <input
-              className={formStyles.input}
-              type="text"
-              {...register("lastName")}
-            />
-          </div>
-          <br />
-          <div className={formStyles.formfield}>
-            <label className={formStyles.label}>Email:</label>
-            <input
-              className={formStyles.input}
-              type="email"
-              {...register("email")}
-            />
-          </div>
-          <button className={buttonStyles.primarybtn} type="submit">
-            Subscribe
-          </button>
-        </form>
+        <div className={formStyles.bodycontainer}>
+          <form
+            /* className={formStyles.formcontainer} */
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className={formStyles.titletext}>
+              <p>Shhhhh welcome to the Secret Club.....</p>
+              <br />
+            </div>
+            <div className={formStyles.text}>
+              <p>
+                If you subscribe to my mailing list, you will be the first to
+                see any available art to tattoo before I post it out there for
+                others to grab.
+              </p>
+              <br />
+            </div>
+            <div className={formStyles.text}>
+              <p>
+                I also run regular promotions that are exclusive to the Secret
+                Club which could be anything from timed discounted slots to
+                special offers.
+              </p>
+              <br />
+            </div>
+            <div className={formStyles.text}>
+              <p>
+                I hate getting spammed in my inbox as much as anyone so the
+                Secret Club emails only go out approximately once a month.
+              </p>
+              <br />
+            </div>
+            <div className={formStyles.text}>
+              <p>
+                Please add my email to your contacts to avoid missing out -
+                <strong>stevek.tattoo@gmail.com</strong>
+              </p>
+              <br />
+            </div>
+            <div className={formStyles.formfield}>
+              <label className={formStyles.label}>First Name:</label>
+              <input
+                className={formStyles.input}
+                type="text"
+                {...register("firstName")}
+              />
+            </div>
+            <br />
+            <div className={formStyles.formfield}>
+              <label className={formStyles.label}>Last Name:</label>
+              <input
+                className={formStyles.input}
+                type="text"
+                {...register("lastName")}
+              />
+            </div>
+            <br />
+            <div className={formStyles.formfield}>
+              <label className={formStyles.label}>Email:</label>
+              <input
+                className={formStyles.input}
+                type="email"
+                {...register("email")}
+              />
+            </div>
+            <button className={buttonStyles.primarybtn} type="submit">
+              Subscribe
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
   );
 };
 export default SubscribeForm;
