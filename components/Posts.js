2675fabe663b663a45f999d764a4c2605f7bc030
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-09-14 18:58:47 +1200

    More work on blog

--- components/Posts.js
+++ components/Posts.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import blogStyles from "../styles/Blog.module.css";
 import { client } from "@/sanity/lib/client";
-import {Link} from "react-router-dom"
+import Link from "next/link";
 import post from "@/sanity/schemas/post";
 const Posts = ({ backgroundImage }) => {
   const mainpageStyle = {
@@ -33,24 +33,30 @@ const Posts = ({ backgroundImage }) => {
     <>
       <section className={blogStyles.mainpage} style={mainpageStyle}>
         <div className={blogStyles.bodycontainer}>
-        <h1 className={blogStyles.boldtext}>Welcome to my world....</h1>
-        <div className={blogStyles.grid}>
-          {postData && postData.map((post, index) => (
-          <article>
-            <Link to={"/blog/" + post.slug.current} key={post.slug.current}>
-              <span className={blogStyles.tile} key={index}>
-                <img 
-                src={post.mainImage.asset.url}
-                alt={post.mainImage.alt}
-                className={blogStyles.postimage}
-                />
-                <span className={blogStyles.tiletitle}>
-                  <h3>{post.title}</h3>
-                </span>
-              </span>
-            </Link>
-          </article>
-          ))}
+          <div className={blogStyles.titleContainer}>
+          <h1 className={blogStyles.boldtext}>Welcome to my world....</h1>
+          </div>
+          <div className={blogStyles.grid}>
+            {postData &&
+              postData.map((post, index) => (
+                <article>
+                  <Link
+                    href={`/blog/${post.slug.current}`}
+                    key={post.slug.current}
+                  >
+                    <span className={blogStyles.tile} key={index}>
+                      <img
+                        src={post.mainImage.asset.url}
+                        alt={post.mainImage.alt}
+                        className={blogStyles.postimage}
+                      />
+                      <span className={blogStyles.tiletitle}>
+                        <h3 className={blogStyles.tiletext}>{post.title}</h3>
+                      </span>
+                    </span>
+                  </Link>
+                </article>
+              ))}
           </div>
         </div>
       </section>
